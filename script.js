87bb01e8a441edeb0fdbb7180497d55ae87e05f8
Bsodoge <kashhh4u@gmail.com>
2022-12-12 18:50:20 +0000

    Add black button

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const container = document.querySelector("#container");
 const button = document.querySelector('#newGrid');
+const gridLines = document.querySelector('#gridLines');
+const black = document.querySelector('#black');
+let toggled = false;
 button.addEventListener('click',newGrid);
+black.addEventListener('click',() =>  console.log("black") );
+gridLines.addEventListener('click',gridLineToggle);
 
 function drawGrid(Length){
     for(let y = 0; y < Length; y++){
@@ -10,17 +15,38 @@ function drawGrid(Length){
         for(let x = 0; x < Length; x++){
             const column = document.createElement('div');
             column.classList.add('column');
-            column.addEventListener("mouseover",() => {column.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`});
+            column.addEventListener("mouseover",() => {column.style.backgroundColor = `rgb(, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`});
             row.appendChild(column);
         }
     }   
 }
 function newGrid(){
     let choice = prompt("Enter number of squares per side (max 100)");
-    if(choice > 100 || choice < 0) return;
+    if(choice > 100 || choice < 0 || choice % 1 !== 0)return;
     while (container.firstChild) { //removes all child nodes
         container.removeChild(container.lastChild);
     }
+    toggled = true
     drawGrid(choice);
+    gridLineToggle();
+}
+function gridLineToggle(){
+    const columns = document.querySelectorAll('.column');
+    if(toggled === false){
+        columns.forEach(column => {
+            column.style.border = "0px";
+        });
+        gridLines.style.backgroundColor = "green";
+        gridLines.textContent = "Enable Gridlines";
+        toggled = true;
+    }
+    else{
+        columns.forEach(column => {
+            column.style.border = "solid 0.25px rgb(209, 209, 209)";
+        });
+        gridLines.style.backgroundColor = "red";
+        gridLines.textContent = "Disable Gridlines";
+        toggled = false;
+    }
 }
 drawGrid(16);
