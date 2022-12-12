f354a9947689e90df54aefc5a1ca3b855ce6909b
Bsodoge <kashhh4u@gmail.com>
2022-12-12 15:55:11 +0000

    Make it fit

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const container = document.querySelector("#container");
-for(let y = 0; y < 16; y++){
-    for(let x = 0; x < 16; x++){
-        const pixel = document.createElement('div');
-        pixel.innerHTML = "hello";
-        container.appendChild(pixel);
-        console.log('help');
+let heightandwidth = 16;
+for(let y = 0; y < heightandwidth; y++){
+    const row = document.createElement('div');
+    row.classList.add('row');
+    container.appendChild(row);
+    for(let x = 0; x < heightandwidth; x++){
+        const column = document.createElement('div');
+        column.classList.add('column');
+        column.addEventListener("mouseover",() => {column.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`});
+        row.appendChild(column);
     }
 }
